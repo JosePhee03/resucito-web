e5604e1cfae00f27693a4ef43f40141d0df06165
Jose Luis Gutierrez <joselugz03@gmail.com>
2023-07-21 23:38:04 -0300

    ✅ fronted: update types

---
+++ @types/canticle.d.ts
@@ -0,0 +1,32 @@
+declare module 'canticle' {
+  export interface Canticle {
+    lyric: string;
+    tone: Chord;
+    capo: number;
+    tags: Tags[];
+    title: string;
+    page: number;
+    stage: Stage;
+    subTitle: string;
+  }
+  
+  export type Chord = string
+  
+  export type Stage = 'election' | 'precatechumenate' | 'catechumenate' | 'liturgy'
+  
+  export type Tags = 
+  | "psalm"
+  | "advent"
+  | "children's song"
+  | "christmas"
+  | "communion"
+  | "lent"
+  | "easter"
+  | "entrance"
+  | "exit"
+  | "fraction of bread"
+  | "lutes and vespers"
+  | "peace and offerings"
+  | "pentecost"
+  | "signing to the virgin"
+}
