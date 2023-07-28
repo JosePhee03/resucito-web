9c11bf3ce210bbb5dca9db1697b80322f532beb0
Jose Luis Gutierrez <joselugz03@gmail.com>
2023-07-28 17:14:13 -0300

    ✅ frontend: update router paths

--- frontend/src/router/paths.ts
+++ frontend/src/router/paths.ts
@@ -3,39 +3,49 @@ import { Route } from '@vaadin/router'
 export const routes: Route[] = [
   {
     path: '/',
-    name: 'home',
-    component: 'page-home',
+    name: 'app',
+    component: 'c-app',
     action: async () => {
-      await import('../pages/home/page-home.js')
-    }
-  },
-  {
-    path: '/canticle',
-    redirect: '/search'
-  },
-  {
-    path: '/search',
-    name: 'search',
-    component: 'page-search',
-    action: async () => {
-      await import('../pages/search/page-search.js')
-    }
-  },
-  {
-    path: '/canticle/:page',
-    name: 'canticle',
-    component: 'page-canticle',
-    action: async () => {
-      await import('../pages/canticle/page-canticle.js')
-    }
-  },
-  {
-    path: '/editor/:page',
-    name: 'editor',
-    component: 'page-editor',
-    action: async () => {
-      await import('../pages/editor/page-editor.js')
-    }
+      await import('../app.js')
+    },
+    children: [
+      {
+        path: '',
+        name: 'home',
+        component: 'page-home',
+        action: async () => {
+          await import('../pages/home/page-home.js')
+        }
+      },
+      {
+        path: '/canticle',
+        redirect: '/search'
+      },
+      {
+        path: '/search',
+        name: 'search',
+        component: 'page-search',
+        action: async () => {
+          await import('../pages/search/page-search.js')
+        }
+      },
+      {
+        path: '/canticle/:page',
+        name: 'canticle',
+        component: 'page-canticle',
+        action: async () => {
+          await import('../pages/canticle/page-canticle.js')
+        }
+      },
+      {
+        path: '/editor/:page',
+        name: 'editor',
+        component: 'page-editor',
+        action: async () => {
+          await import('../pages/editor/page-editor.js')
+        }
+      }
+    ]
   },
   {
     path: '(.*)',
