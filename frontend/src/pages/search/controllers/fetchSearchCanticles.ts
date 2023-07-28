dc9d794d282bded9f4594adea1d7139fae111267
Jose Luis Gutierrez <joselugz03@gmail.com>
2023-07-28 17:15:56 -0300

    ✅ frontend: update fetch search canticle

--- frontend/src/pages/search/controllers/fetchSearchCanticles.ts
+++ frontend/src/pages/search/controllers/fetchSearchCanticles.ts
@@ -1,9 +1,9 @@
 import { searchCanticles } from '@/services/searchCanticle'
 import { getSearchQuery } from '@/util/queryHandle'
 import { Canticle } from 'canticle'
-import { ReactiveControllerHost } from 'lit'
+import { ReactiveController, ReactiveControllerHost } from 'lit'
 
-export class FetchSearchCanticles {
+export class FetchSearchCanticles implements ReactiveController {
   canticles: Canticle[] = []
   isLoading: boolean = false
   isError: boolean = false
@@ -12,27 +12,40 @@ export class FetchSearchCanticles {
   tags: string = ''
   stage: string = ''
   q: string = ''
+  total: number = 0
   readonly host: ReactiveControllerHost
 
   constructor (host: ReactiveControllerHost) {
     (this.host = host).addController(this)
+  }
+
+  hostConnected () {
+    window.addEventListener('vaadin-router-location-changed', this._updateNavigation.bind(this))
+  }
+
+  hostDisconnected () {
+    window.removeEventListener('vaadin-router-location-changed', this._updateNavigation.bind(this))
+  }
+
+  _updateNavigation () {
+    this.canticles = []
+    this.skip = 0
+    this._fetchData()
+  }
+
+  _fetchData () {
     const { stage, tags, q } = getSearchQuery()
     this.tags = tags
     this.stage = stage
     this.q = q
-  }
-
-  hostConnected () {
-  }
-
-  _fetchData () {
     const response = searchCanticles(this.q, this.stage, this.tags, this.skip, this.limit)
     this.isLoading = true
     response
       .then(res => {
+        console.log(res)
+        this.total = res.length
         if (this.skip <= res.length + this.limit) { this.canticles.push(...res.canticles) }
         this.skip += this.limit
-        console.log({ canticle: this.canticles, newCanticle: res.canticles })
       })
       .catch(e => {
         console.log(e)
